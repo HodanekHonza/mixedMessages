36bb7c190e87f0e99a9256e584aef55c86dabf47
Jan Hodánek <hodanekk@gmail.com>
2022-08-04 20:15:42 +0200

    hello

---
+++ app.js
@@ -0,0 +1,5 @@
+arr1 = ["you have", "your attack", "saying goes:"];
+arr2 = ["been killed", "went wrong", "your mum has best cookies" ];
+
+randInt1 = Math.floor(Math.random * length.arr1)
+randInt2 = Math.floor(Math.random * length.arr2)

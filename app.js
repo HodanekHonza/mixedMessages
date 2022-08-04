d9f954dd535ba2cf8da4173065d5f5dd72b3fd42
Jan Hodánek <hodanekk@gmail.com>
2022-08-04 21:08:28 +0200

    function with switch and randint

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 arr1 = ["you have", "your attack", "saying goes:"];
 arr2 = ["been killed", "went wrong", "your mum has best cookies" ];
 
-randInt1 = Math.floor(Math.random * arr1.length)
-randInt2 = Math.floor(Math.random * arr2.length)
 
-switch (randInt1) {
-    case 0: 
-        string = arr1[0];
-        console.log(string);
-        break;
-    case 1: 
-        string = arr1[1];
-        console.log(string);
-        break;
-    case 2: 
-        string = arr1[2];
-        console.log(string);
-        break;
+const gettingBlasted = () => {
+    randInt1 = Math.floor(Math.random() * arr1.length)
+    console.log(randInt1)
+    randInt2 = Math.floor(Math.random() * arr2.length)
+    console.log(randInt2)
+    stringRand1 = "";
+    stringRand2 = "";
+    switch (randInt1) {
+        case 0: 
+            stringRand1 = arr1[0];
+            break;
+        case 1: 
+        stringRand1 = arr1[1];
+            break;
+        case 2: 
+        stringRand1 = arr1[2];
+            break;
+    }
+    switch (randInt2) {
+        case 0: 
+        stringRand2 = arr2[0];
+            break;
+        case 1: 
+        stringRand2 = arr2[1];
+            break;
+        case 2: 
+        stringRand2 = arr2[2];
+            break;
+    }
+    stringsTogether = stringRand1 + " " + stringRand2;
+    console.log(stringsTogether)
+
 }
+
+gettingBlasted();
